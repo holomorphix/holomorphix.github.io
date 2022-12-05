26e16907f343cb48353c5f6527866d4fa519870e
holomorphix <holomorphix@gmail.com>
2022-12-04 22:21:46 -0700

    Update and rename relay-cnode.sh to cnode.sh

--- cnode.sh
+++ cnode.sh
@@ -14,8 +14,14 @@ SOCKET=${DB}/node.socket
 # Port to run node on
 PORT=6969
 
-# Local IP address (set to 0.0.0.0 to listen on all local addresses)
+# Local IP address 
 HOST=0.0.0.0
 
 # Run node
 /home/holo/.local/bin/cardano-node run --host-addr ${HOST} --port ${PORT} --database-path ${DB} --socket-path ${SOCKET} --config ${CONFIG} --topology ${TOPO}
+
+# Implement lines below to run as core node
+# Path to stake pool keys, addresses, and certificates
+#POOL=<path to pool directory>
+# Additional run command options
+#--shelley-kes-key ${POOL}/kes.skey --shelley-vrf-key ${POOL}/vrf.skey --shelley-operational-certificate ${POOL}/op.cert
